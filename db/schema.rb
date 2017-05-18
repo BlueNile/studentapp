ea5a9fbd5dfdc941acbb84289a3c4ff618066876
waleednsr <zorooking22@gmail.com>
2017-05-18 17:36:03 +0000

    install bootstrap sass

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170518171750) do
+
+  create_table "stages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "timeofcourse"
+    t.string   "type"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
